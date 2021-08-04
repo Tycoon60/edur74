061c2eac84dc38e96ea3d1c048cce4767c04e6da
Tycoon60 <72808269+Tycoon60@users.noreply.github.com>
2021-08-04 17:09:06 +0500

    Update check_arp.sh

--- check_arp.sh
+++ check_arp.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-while :
-do
+filter01=$(netstat -i | grep eth0 | awk '{print $1}')
 
-sleep 60
+if [ -n "$filter01" ]; then
 
-date >> /tmp/logarp.txt
-arp -n -i eth0 >> /tmp/logarp.txt
+	while :
+	do
 
-done
+	sleep 60
+
+	date >> /tmp/logarp.txt
+	arp -n -i eth0 >> /tmp/logarp.txt
+
+	done
+
+else
+	echo "Интерфейс eth0 не найден" >> /tmp/logarp.txt
+	echo "script check_arp done" >> /tmp/logarp.txt
+	break
+fi
